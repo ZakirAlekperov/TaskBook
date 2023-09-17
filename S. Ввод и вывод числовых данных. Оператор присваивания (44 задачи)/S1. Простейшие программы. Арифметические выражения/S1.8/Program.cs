80f03c730da4b2b6dcb8ffa9d19476bce0e15861
Закир Алекперов <zakiralekperov1998@gmail.com>
2023-09-17 23:21:02 +0300

    S1.8. Составить программу вывода на экран в одну строку четырех любых чисел  с одним пробелом между ними.

---
+++ S. Ввод и вывод числовых данных. Оператор присваивания (44 задачи)/S1. Простейшие программы. Арифметические выражения/S1.8/Program.cs
@@ -0,0 +1,8 @@
+﻿//S1.8. Составить программу вывода на экран в одну строку четырех любых чисел  с одним пробелом между ними.
+
+double a = new Random().NextDouble();
+double b = new Random().NextDouble();
+double c = new Random().NextDouble();
+double d = new Random().NextDouble();
+
+Console.WriteLine(a+" "+b+" "+c+" "+d);
