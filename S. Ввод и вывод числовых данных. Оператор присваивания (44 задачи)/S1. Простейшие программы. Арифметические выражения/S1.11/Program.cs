1763ec0b80dd9bc36d4bd9e682ba558f437db2bf
Закир Алекперов <zakiralekperov1998@gmail.com>
2023-09-17 23:25:47 +0300

    S1.11. Составить программу вывода на экран «столбиком» четырех любых чисел.

---
+++ S. Ввод и вывод числовых данных. Оператор присваивания (44 задачи)/S1. Простейшие программы. Арифметические выражения/S1.11/Program.cs
@@ -0,0 +1,8 @@
+﻿// S1.11. Составить программу вывода на экран «столбиком» четырех любых чисел.
+
+double a = new Random().NextDouble();
+double b = new Random().NextDouble();
+double c = new Random().NextDouble();
+double d = new Random().NextDouble();
+
+Console.WriteLine(a + "\n" + b + "\n" + c + "\n" + d);
