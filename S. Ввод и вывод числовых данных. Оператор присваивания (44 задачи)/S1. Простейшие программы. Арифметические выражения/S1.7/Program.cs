35e1cac59b878f86a26ef9a35cf9e2e227633eab
Закир Алекперов <zakiralekperov1998@gmail.com>
2023-09-17 23:19:07 +0300

    S1.7. Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.

---
+++ S. Ввод и вывод числовых данных. Оператор присваивания (44 задачи)/S1. Простейшие программы. Арифметические выражения/S1.7/Program.cs
@@ -0,0 +1,6 @@
+﻿//S1.7. Составить программу вывода на экран в одну строку трех любых чисел с двумя пробелами между ними.
+double a = new Random().Next();
+double b = new Random().Next();
+double c = new Random().Next();
+
+Console.WriteLine(a + "  " + b + "  " + c);
